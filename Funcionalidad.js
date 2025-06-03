66484d383766e4e9b50de4ac17b42946ce954d91
Eiddy29 <eepaez@ucundinamarca.edu.co>
2025-06-03 10:31:57 -0500

    cambios

---
+++ Funcionalidad.js
@@ -0,0 +1,15 @@
+function asistencia() {
+    window.location.href = "frontend/Asistencia.html";
+}
+
+function asignatura() {
+    window.location.href = "frontend/Asignatura.html";
+}
+
+function estudiante() {
+    window.location.href = "frontend/Estudiante.html";
+}
+
+function departamento() {
+    window.location.href = "frontend/Departamento.html";
+}
